383c78c29db28d1a06ce459eb4a24db5fed8c3ad
Rien Heuver <rienheuver@gmail.com>
2016-07-30 22:04:54 +0200

    Attempt 2 at #3

--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -7,8 +7,8 @@
             @foreach ($rows as $index => $row)
                 <tr>
                     <td><input type="hidden" name="transactions[{{ $index }}][date]" value="{{ $row['date']->format('d-m-Y') }}">{{ $row['date']->format('d-m-Y') }}</td>
-                    <td><input type="hidden" name="transactions[{{ $index }}][amount]" value="{{ $row['amount'] }}">{{ explode(" - ",$row['amount'])[0] }} - <p class="text-muted">{{ explode(" - ",$row['amount'])[1] }}</td>
-                    <td><input type="hidden" name="transactions[{{ $index }}][title]" value="{{ $row['title'] }}">{{ $row['title'] }}</td>
+                    <td><input type="hidden" name="transactions[{{ $index }}][amount]" value="{{ $row['amount'] }}">{{ $row['amount'] }}</td>
+                    <td><input type="hidden" name="transactions[{{ $index }}][title]" value="{{ $row['title'] }}">{{ explode(" - ",$row['title'])[0] }} - <p class="text-muted">{{ explode(" - ",$row['title'])[1] }}</td>
                     <td>
                         <div class="checkbox">
                             <input type="hidden" name="transactions[{{ $index }}][hash]" value="{{ $row['hash'] }}">
