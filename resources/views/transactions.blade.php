bf0d96c192b9e7a097b784a0543769006ccb3016
Joanne <eagle11.1.010@gmail.com>
2016-11-11 21:55:45 +0100

    Amount of transactions /100

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -7,7 +7,7 @@
     @foreach ($rows['joanne'] as $index => $row)
         <tr>
             <td><input type="hidden" name="transactions[{{ $index }}][date]" value="{{ $row['date']->format('d-m-Y') }}">{{ $row['date']->format('d-m-Y') }}</td>
-            <td><input type="hidden" name="transactions[{{ $index }}][amount]" value="{{ $row['amount'] }}">&euro;{{ $row['amount'] }}</td>
+            <td><input type="hidden" name="transactions[{{ $index }}][amount]" value="{{ $row['amount'] }}">&euro;{{ $row['amount']/100 }}</td>
             <td><input type="hidden" name="transactions[{{ $index }}][title]" value="{{ $row['title'] }}">{{ explode(" - ",$row['title'])[0] }}<p class="text-muted">{{ explode(" - ",$row['title'])[1] }}</td>
         </tr>
     @endforeach
@@ -18,7 +18,7 @@
     @foreach ($rows['rien'] as $index => $row)
         <tr>
             <td><input type="hidden" name="transactions[{{ $index }}][date]" value="{{ $row['date']->format('d-m-Y') }}">{{ $row['date']->format('d-m-Y') }}</td>
-            <td><input type="hidden" name="transactions[{{ $index }}][amount]" value="{{ $row['amount'] }}">&euro;{{ $row['amount'] }}</td>
+            <td><input type="hidden" name="transactions[{{ $index }}][amount]" value="{{ $row['amount'] }}">&euro;{{ $row['amount']/100 }}</td>
             <td><input type="hidden" name="transactions[{{ $index }}][title]" value="{{ $row['title'] }}">{{ explode(" - ",$row['title'])[0] }}<p class="text-muted">{{ explode(" - ",$row['title'])[1] }}</td>
         </tr>
     @endforeach
